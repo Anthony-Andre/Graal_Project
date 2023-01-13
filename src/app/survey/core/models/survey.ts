f9eb273e11d418951a18ce38bddd59f60c1ff111
loicgardes <loic.gardes@gmail.com>
2023-01-13 11:39:02 +0100

    SurveyModel, SurveyDto,  SurveyService

---
+++ src/app/survey/core/models/survey.ts
@@ -0,0 +1,46 @@
+import { Level } from "src/app/core/enums/level";
+import { PoeType } from "src/app/core/enums/poe-type";
+
+export class Survey {
+    private id: number = 0;
+    private title: string = "";
+    private type!: PoeType;
+    private level!: Level;
+    
+
+
+    public getId(): number {
+        return this.id;
+    }
+
+    public setId(id: number): void {
+        this.id = id;
+    }
+
+    public getTitle(): string {
+        return this.title;
+    }
+
+    public setTitle(title: string): void {
+        this.title = title;
+    }
+
+    public getType(): PoeType {
+        return this.type;
+    }
+
+    public setPoeType(type: PoeType): void {
+        this.type = type;
+    }
+
+    public getLevel(): PoeType {
+        return this.type;
+    }
+
+    public setLevel(type: PoeType): void {
+        this.type = type;
+    }
+
+
+   
+}
