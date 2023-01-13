185c875055f7feb38e8f0cc75e3d6cd44b8d6055
anthony.andre <anthony-andre@live.fr>
2023-01-13 17:19:31 +0100

    commitAvantWeekEnd

--- src/app/core/poes/components/poe-details/poe-details.component.ts
+++ src/app/core/poes/components/poe-details/poe-details.component.ts
@@ -103,6 +103,8 @@ export class PoeDetailsComponent implements OnInit {
   }
 
   public addNewTrainee() {
+
+    const choixTrainee = console.log(document.getElementById('choixTrainee'));
     console.log("L'utilisateur veut ajouter un nouveau stagiaire");
 
     this.selectHidden = true;
