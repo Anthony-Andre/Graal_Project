0e62bc5e27d362e0f6df46a82b1ad33254ab8d44
loicgardes <loic.gardes@gmail.com>
2023-01-15 21:41:55 +0100

    Create Question Model,Dto and Service

---
+++ src/app/question/core/models/question.ts
@@ -0,0 +1,34 @@
+import { AnswerType } from "src/app/core/enums/AnswerType";
+
+export class Question {
+    private id: number = 0;
+    private text: string = "";
+    private answerType!: AnswerType;
+
+
+
+    
+    public getId(): number {
+        return this.id;
+    }
+
+    public setId(id: number): void {
+        this.id = id;
+    }
+
+    public getText(): string {
+        return this.text;
+    }
+
+    public setText(text: string): void {
+        this.text = text;
+    }
+
+    public getAnswerType(): AnswerType {
+        return this.answerType;
+    }
+
+    public setAnswerType(answerType: AnswerType): void {
+        this.answerType = answerType;
+    }
+}
