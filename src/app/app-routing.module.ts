55dcc1868ef31760b3eda3f18a496462faec06be
RaphD <111575644+RaphD31@users.noreply.github.com>
2023-01-17 14:53:03 +0100

    surveyTable rooting ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,7 @@ import { SignupFormComponent } from './user/components/signup-form/signup-form.c
 import { PoeDetailsComponent } from './core/poes/components/poe-details/poe-details.component';
 import { SurveyFormComponent } from './survey/components/survey-form/survey-form.component';
 import { QuestionFormComponent } from './question/components/question-form/question-form/question-form.component';
+import { SurveyTableComponent } from './survey/components/survey-table/survey-table.component';
 
 const routes: Routes = [];
 
@@ -127,7 +128,13 @@ export class AppRoutingModule {
         HasUserGuard
       ]
     },
-    
+    {
+      path: 'survey-table',
+      component: SurveyTableComponent,
+      canActivate: [
+        HasUserGuard
+      ]
+    },
     {
       path: '**', // Wild card (Redirige vers le home si l'url n'existe pas) - Toujours mettre en dernier
       redirectTo: 'home',
