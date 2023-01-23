c439a314e94bbd55c492ad3269d00da6bf352637
RaphD <111575644+RaphD31@users.noreply.github.com>
2023-01-23 10:52:21 +0100

    ajout de l'attribut poe[] dans survey

--- src/app/survey/dto/survey-dto.ts
+++ src/app/survey/dto/survey-dto.ts
@@ -1,5 +1,6 @@
 import { Level } from "src/app/core/enums/level";
 import { PoeType } from "src/app/core/enums/poe-type";
+import { Poe } from "src/app/core/models/poe";
 import { Question } from "src/app/question/core/models/question";
 
 export class SurveyDto {
@@ -8,12 +9,14 @@ export class SurveyDto {
   public poeType!: PoeType;
   public level!: Level;
   public questions: Array<Question> = [];
+  public poes: Array<Poe> = [];
 
 
   public constructor(formValues: any) {
     Object.assign(this, formValues);
     // this.poeType = formValues.type; 
   }
+
 
   public setQuestions(questions: Array<Question>) {
     this.questions = questions;
