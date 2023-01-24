5b9407ac4b47c7eaed0b7fbf77d46b9cb068fdac
loicgardes <loic.gardes@gmail.com>
2023-01-24 16:49:40 +0100

    Creation of Message-Answer

---
+++ src/app/answer/components/message-answer/message-answer.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MessageAnswerComponent } from './message-answer.component';
+
+describe('MessageAnswerComponent', () => {
+  let component: MessageAnswerComponent;
+  let fixture: ComponentFixture<MessageAnswerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MessageAnswerComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MessageAnswerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
